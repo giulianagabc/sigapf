0c758e244ef69d967e146db902f3618da47fa66c
giulianagabc <giuliana.bcosta@gmail.com>
2016-03-07 14:07:39 -0300

    Inclusão da regra de negócio do IFPUG

--- src/main/java/br/fatec/sigapf/service/impl/ItemPlanilhaServiceImpl.java
+++ src/main/java/br/fatec/sigapf/service/impl/ItemPlanilhaServiceImpl.java
@@ -68,7 +68,7 @@ public class ItemPlanilhaServiceImpl implements ItemPlanilhaService {
 	}
 
 	// TODO - valor do deflator para multiplicar na qtde pf total
-	// TODO - ITENS DE DADOS
+	// TODO - receber o valor do deflator da planilha
 	@Override
 	public ItemPlanilha verificarPontuacao(
 			ItemPlanilha itemPlanilhaParaVerificacao) {
@@ -109,69 +109,443 @@ public class ItemPlanilhaServiceImpl implements ItemPlanilhaService {
 			if (itemPlanilhaParaVerificacao.getTipoFuncao().getSigla()
 					.equals("ALI")) {
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() < 2) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 20)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 50)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 50) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(10);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(10);
+					}
 				}
 				if ((itemPlanilhaParaVerificacao.getQuantidadeRl() >= 2)
 						|| (itemPlanilhaParaVerificacao.getQuantidadeRl() <= 5)) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 20)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 50)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(10);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(10);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 50) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(15);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(15);
+					}
 				}
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() > 5) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(10);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(10);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 20)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 50)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(15);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(15);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 50) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(15);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(15);
+					}
 				}
 			}
 			if (itemPlanilhaParaVerificacao.getTipoFuncao().getSigla()
 					.equals("AIE")) {
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() < 2) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 20)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 50)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 50) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(10);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(10);
+					}
 				}
 				if ((itemPlanilhaParaVerificacao.getQuantidadeRl() >= 2)
 						|| (itemPlanilhaParaVerificacao.getQuantidadeRl() <= 5)) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 20)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 50)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(10);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(10);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 50) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(15);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(15);
+					}
 				}
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() > 5) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(10);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(10);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 20)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 50)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(15);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(15);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 50) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(15);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(15);
+					}
 				}
 			}
 			if (itemPlanilhaParaVerificacao.getTipoFuncao().getSigla()
 					.equals("EE")) {
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() < 2) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 4)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(3);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(3);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 5)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 15)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(3);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(3);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 15) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
 				}
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() == 2) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 4)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(3);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(3);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 5)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 15)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 15) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(6);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(6);
+					}
 				}
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() > 2) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 4)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 5)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 15)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(6);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(6);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 15) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(6);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(6);
+					}
 				}
 			}
 			if (itemPlanilhaParaVerificacao.getTipoFuncao().getSigla()
 					.equals("CE")) {
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() < 2) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 5)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(3);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(3);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 6)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(3);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(3);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 19) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
 				}
 				if ((itemPlanilhaParaVerificacao.getQuantidadeRl() >= 2)
 						|| (itemPlanilhaParaVerificacao.getQuantidadeRl() <= 3)) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 5)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(3);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(3);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 6)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 19) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(6);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(6);
+					}
 				}
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() > 3) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 5)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 6)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(6);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(6);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 19) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(6);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(6);
+					}
 				}
 			}
 			if (itemPlanilhaParaVerificacao.getTipoFuncao().getSigla()
 					.equals("SE")) {
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() < 2) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 5)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 6)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 19) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(5);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(5);
+					}
 				}
 				if ((itemPlanilhaParaVerificacao.getQuantidadeRl() >= 2)
 						|| (itemPlanilhaParaVerificacao.getQuantidadeRl() <= 3)) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 5)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.BAIXA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(4);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(4);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 6)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(5);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(5);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 19) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
 				}
 				if (itemPlanilhaParaVerificacao.getQuantidadeRl() > 3) {
-
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 1)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 5)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.MEDIA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(5);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(5);
+					}
+					if ((itemPlanilhaParaVerificacao.getQuantidadeTd() >= 6)
+							&& (itemPlanilhaParaVerificacao.getQuantidadeTd() <= 19)) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
+					if (itemPlanilhaParaVerificacao.getQuantidadeTd() > 19) {
+						itemPlanilhaParaVerificacao
+								.setComplexidade(ComplexidadeItemPlanilha.ALTA);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoLocal(7);
+						itemPlanilhaParaVerificacao
+								.setQuantidadePontoFuncaoTotal(7);
+					}
 				}
 			}
 		}
 		return itemPlanilhaParaVerificacao;
 	}
-
 }
